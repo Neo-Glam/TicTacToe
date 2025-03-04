cfab54f933fdba86ab7dabf64809e67abf2229b3
gabriel <gabds.123@gmail.com>
2025-03-04 22:25:40 +0100

    ajoute les conditions de victoire et d'egalite

--- script.js
+++ script.js
@@ -1,69 +1,86 @@
 const game =(function() {
 
     const gameboard = {
-        gameboard : ["","","","","","","","",""]
-    };
+        gameboard : ["","","","","","","","",""],
+        winner : "",
+        counter : 0,
 
-    const player = (name,mark)=>{
+        reset : ()=>{
+            return gameboard.gameboard = ["","","","","","","","",""] , gameboard.winner = "";
+        },
 
-        const gameOver = ()=>{
-            if (gameboard.gameboard[0] == gameboard.gameboard[1] && 
-                gameboard.gameboard[0] == gameboard.gameboard[2] &&
+        gameOver : ()=>{
+            if (gameboard.gameboard[0] === gameboard.gameboard[1] && 
+                gameboard.gameboard[0] === gameboard.gameboard[2] &&
                 gameboard.gameboard[0] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[3] == gameboard.gameboard[4] && 
-                gameboard.gameboard[3] == gameboard.gameboard[5] &&
+            else if (gameboard.gameboard[3] === gameboard.gameboard[4] && 
+                gameboard.gameboard[3] === gameboard.gameboard[5] &&
                 gameboard.gameboard[3] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[6] == gameboard.gameboard[7] && 
-                gameboard.gameboard[6] == gameboard.gameboard[7] &&
+            else if (gameboard.gameboard[6] === gameboard.gameboard[7] && 
+                gameboard.gameboard[6] === gameboard.gameboard[7] &&
                 gameboard.gameboard[6] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[0] == gameboard.gameboard[4] && 
-                gameboard.gameboard[0] == gameboard.gameboard[8] &&
+            else if (gameboard.gameboard[0] === gameboard.gameboard[4] && 
+                gameboard.gameboard[0] === gameboard.gameboard[8] &&
                 gameboard.gameboard[0] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[2] == gameboard.gameboard[4] && 
-                gameboard.gameboard[2] == gameboard.gameboard[6] &&
+            else if (gameboard.gameboard[2] === gameboard.gameboard[4] && 
+                gameboard.gameboard[2] === gameboard.gameboard[6] &&
                 gameboard.gameboard[2] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[0] == gameboard.gameboard[3] && 
-                gameboard.gameboard[0] == gameboard.gameboard[6] &&
+            else if (gameboard.gameboard[0] === gameboard.gameboard[3] && 
+                gameboard.gameboard[0] === gameboard.gameboard[6] &&
                 gameboard.gameboard[3] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[1] == gameboard.gameboard[4] && 
-                gameboard.gameboard[1] == gameboard.gameboard[4] &&
+
+            else if (gameboard.gameboard[1] === gameboard.gameboard[4] && 
+                gameboard.gameboard[1] === gameboard.gameboard[7] &&
                 gameboard.gameboard[1] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-            else if (gameboard.gameboard[2] == gameboard.gameboard[5] && 
-                gameboard.gameboard[2] == gameboard.gameboard[8] &&
+            else if (gameboard.gameboard[2] === gameboard.gameboard[5] && 
+                gameboard.gameboard[2] === gameboard.gameboard[8] &&
                 gameboard.gameboard[2] != ""
             ){
                 console.log(name," won!" );
-                gameboard.gameboard = ["","","","","","","","",""];
+                gameboard.winner = name;
+                console.log(gameboard.winner)
             }
-        };
+            else if (gameboard.counter >= 9)
+                console.log("draw!");
+        }
+    };
+
+    const player = (name,mark)=>{
 
         const play = (coord)=>{
             const coordinate = {
@@ -76,7 +93,9 @@ const game =(function() {
             if(gameboard.gameboard[index] == ""){
                 gameboard.gameboard[index] = mark;
                 console.log(name,"turn", gameboard.gameboard);
-                gameOver();
+                gameboard.counter += 1;
+                console.log(gameboard.counter);
+                gameboard.gameOver();
             }
             else
                 console.log("invalid case");
@@ -85,6 +104,29 @@ const game =(function() {
     };
     return {player}
 })();
+
+const player1 = game.player("bob","X");
+const player2 = game.player("maria","O");
+
+/*player1.play("a1")
+player2.play("a2")
+player1.play("a3")
+player2.play("b2")
+player1.play("b1")
+player2.play("b3")
+player1.play("c3")
+player2.play("c1")
+player1.play("c2")
+*/
+
+
+
+
+
+
+
+
+
 
 
 
