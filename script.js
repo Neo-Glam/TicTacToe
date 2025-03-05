a160b332a44d8cf291ef2a0ef0d19968988b503f
gabriel <gabds.123@gmail.com>
2025-03-05 17:53:31 +0100

    termine l'interface du jeu

--- script.js
+++ script.js
@@ -1,61 +1,79 @@
 const game =(function() {
 
+    /*     gameboard module */ 
+
     const gameboard = {
         gameboard : ["","","","","","","","",""],
         winner : "",
         counter : 0,
+        lastPlayed : "",
+        playerOne : "",
+        playerTwo : "",   
 
         reset : ()=>{
-            return gameboard.gameboard = ["","","","","","","","",""] , gameboard.winner = "";
+            const Case = document.querySelectorAll(".case").forEach(Case =>
+                Case.innerHTML = ""
+            );
+            return gameboard.counter = 0, gameboard.gameboard = ["","","","","","","","",""], gameboard.winner = "", gameboard.lastPlayed = "", gameboard.playerOne = "", gameboard.playertwo = "";
         },
 
         gameOver : ()=>{
+
+            const winnerScreen = document.querySelector("#winnerScreen");
+
+
             if (gameboard.gameboard[0] === gameboard.gameboard[1] && 
                 gameboard.gameboard[0] === gameboard.gameboard[2] &&
                 gameboard.gameboard[0] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[3] === gameboard.gameboard[4] && 
                 gameboard.gameboard[3] === gameboard.gameboard[5] &&
                 gameboard.gameboard[3] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[6] === gameboard.gameboard[7] && 
                 gameboard.gameboard[6] === gameboard.gameboard[7] &&
                 gameboard.gameboard[6] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[0] === gameboard.gameboard[4] && 
                 gameboard.gameboard[0] === gameboard.gameboard[8] &&
                 gameboard.gameboard[0] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[2] === gameboard.gameboard[4] && 
                 gameboard.gameboard[2] === gameboard.gameboard[6] &&
                 gameboard.gameboard[2] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[0] === gameboard.gameboard[3] && 
                 gameboard.gameboard[0] === gameboard.gameboard[6] &&
                 gameboard.gameboard[3] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
 
@@ -63,24 +81,89 @@ const game =(function() {
                 gameboard.gameboard[1] === gameboard.gameboard[7] &&
                 gameboard.gameboard[1] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
             else if (gameboard.gameboard[2] === gameboard.gameboard[5] && 
                 gameboard.gameboard[2] === gameboard.gameboard[8] &&
                 gameboard.gameboard[2] != ""
             ){
-                console.log(name," won!" );
-                gameboard.winner = name;
+                gameboard.winner = gameboard.lastPlayed;
+                winnerScreen.innerHTML = gameboard.winner + " Won!";
+                winnerModal.showModal();
                 console.log(gameboard.winner)
             }
-            else if (gameboard.counter >= 9)
-                console.log("draw!");
+            else if (gameboard.counter >= 9){
+                winnerScreen.innerHTML = "DRAW!";
+                winnerModal.showModal();
+            }
         }
     };
 
+    const Case = document.querySelectorAll(".case").forEach(Case =>
+        Case.addEventListener("click",()=>{
+            if(gameboard.counter % 2 === 0){
+                gameboard.playerOne.play(Case.id);
+            }
+            else{
+                gameboard.playerTwo.play(Case.id);
+            }
+
+        })
+    );  
+
+    const closeWinnerModal = document.querySelector("#winnerModal")
+    closeWinnerModal.addEventListener("click",()=>{
+        winnerModal.close();
+        gameboard.reset();
+    })
+
+
+
+    /*    addplayer modal module */ 
+
+    const openAddPLayerModal = document.querySelector("#openAddPLayerModal");
+    const closeAddPLayerModal = document.querySelector("#closeAddPLayerModal");
+    const newPlayerForm = document.querySelector("#newPLayerForm")
+
+    openAddPLayerModal.addEventListener("click",()=>{addPLayerModal.showModal()});
+    closeAddPLayerModal.addEventListener("click",()=>{
+        addPLayerModal.close()});
+
+    newPlayerForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        const formData = new FormData(newPlayerForm);
+        const playerOne = { name : formData.get("playerOne"),
+                            mark : "X"                
+                        };
+
+        const playerTwo = { name : formData.get("playerTwo"),
+                            mark : "O"                
+                        };
+
+        gameboard.reset();
+        gameboard.playerOne = player(playerOne.name,playerOne.mark);
+        gameboard.playerTwo = player(playerTwo.name,playerTwo.mark);
+        newPlayerForm.reset();
+        addPLayerModal.close();
+    });
+    
+
+    /*     create player module */
     const player = (name,mark)=>{
+
+        const Case = document.querySelectorAll(".case")
+
+        if (mark === "X"){
+            console.log("Player One: " + name);
+        }
+        else if (mark === "O"){
+            console.log("Player Two: " + name);
+
+        }
 
         const play = (coord)=>{
             const coordinate = {
@@ -90,25 +173,33 @@ const game =(function() {
             }
             const index = coordinate[coord];
 
-            if(gameboard.gameboard[index] == ""){
+            if (gameboard.lastPlayed === name){
+                console.log("it's not your turn yet!");
+            }
+
+            else if(gameboard.gameboard[index] == ""){
                 gameboard.gameboard[index] = mark;
+                Case[index].innerHTML = mark;
                 console.log(name,"turn", gameboard.gameboard);
+                gameboard.lastPlayed = name;
                 gameboard.counter += 1;
-                console.log(gameboard.counter);
                 gameboard.gameOver();
+
             }
             else
                 console.log("invalid case");
         };
         return {play};
     };
-    return {player}
 })();
 
-const player1 = game.player("bob","X");
-const player2 = game.player("maria","O");
 
-/*player1.play("a1")
+/* player1.play("a1");
+player1.play("a2"); */
+
+
+
+/* player1.play("a1")
 player2.play("a2")
 player1.play("a3")
 player2.play("b2")
@@ -116,8 +207,7 @@ player1.play("b1")
 player2.play("b3")
 player1.play("c3")
 player2.play("c1")
-player1.play("c2")
-*/
+player1.play("c2") */
 
 
 
